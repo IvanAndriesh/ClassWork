672ba85933929b0ebcd8a668ccbd04c961c70065
IvanAndriesh <157492101+IvanAndriesh@users.noreply.github.com>
2024-02-02 11:02:58 +0300

    Практическая 1 02.02.2024

---
+++ Классная работа практическая1 02.02.2024.py
@@ -0,0 +1,10 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+
+
+big_netseloe = tseloe * 3.5
+drobnoe -= 1
+print(tseloe/drobnoe, big_netseloe/drobnoe)
+stroka = "No"*3+"_"+"Yes"*3
+print(tseloe, drobnoe, stroka, big_netseloe)
